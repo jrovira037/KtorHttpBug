243d39163ac37ebf23ac2936fcd5c8839763e16e
jrovira037 <rovirajosep37@gmail.com>
2021-01-04 12:32:04 +0100

    Add code

---
+++ src/com/jota/ktor/Application.kt
@@ -0,0 +1,22 @@
+package com.jota.ktor
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    routing {
+        route("test") {
+            get("") {
+                call.respond("Success")
+            }
+        }
+    }
+}
+
